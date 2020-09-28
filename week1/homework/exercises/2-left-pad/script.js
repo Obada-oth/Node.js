a163cedee5ccebb889248ad799afc49f01323b61
obada othman <obada.oth@gmail.com>
2020-09-28 14:04:33 +0200

    finished ex.2 to the left

--- week1/homework/exercises/2-left-pad/script.js
+++ week1/homework/exercises/2-left-pad/script.js
@@ -1,13 +1,14 @@
 /**
  ** Exercise 2: To the left, to the left...
- * 
+ *
  * Copy and paste your code from the previous exercise.
  * Replace the function `padLeft` to use
  * this new NPM package called `left-pad` instead then
  * Pad the numbers to 8 characters to confirm that it works correctly
  *
  */
-
-let numbers = [ "12", "846", "2", "1236" ];
+const lefPad = require("left-pad");
+let numbers = ["12", "846", "2", "1236"];
 
 // YOUR CODE GOES HERE
+numbers.forEach((number) => console.log(lefPad(number, 8, "0")));
