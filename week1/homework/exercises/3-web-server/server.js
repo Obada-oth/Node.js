f95e2520dee01ed35c054a74f1b6336726ab933b
obada othman <obada.oth@gmail.com>
2020-09-28 16:14:36 +0200

    finished ex.3 My first server

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,13 +2,38 @@
  * Exercise 3: Create an HTTP web server
  */
 
-var http = require('http');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const moment = require("moment");
 
 //create a server
-let server = http.createServer(function (req, res) {
+let server = http.createServer((req, res) => {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(content); // Ends the response
+      }
+    );
+  } else if (req.url === "/index.js") {
+    fs.readFile(path.join(__dirname, "public", "index.js"), (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { "content-type": "text/js" });
+      res.end(content); // Ends the response
+    });
+  } else if (req.url === "/style.css") {
+    fs.readFile(path.join(__dirname, "public", "style.css"), (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { "content-type": "text/css" });
+      res.end(content); // Ends the response
+    });
+  }
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+const port = process.env.PORT || 3000;
+
+server.listen(port); // The server starts to listen on port 3000
