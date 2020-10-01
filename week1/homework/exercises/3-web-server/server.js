c58ccf8a6bc744538c8fb4ab7f530bb7370d2435
obada othman <obada.oth@gmail.com>
2020-10-01 21:35:37 +0200

    made changes suggested by reviewer , refactored and DRYed code in ex 3-wev-server

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -5,33 +5,63 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const moment = require("moment");
 
 //create a server
 let server = http.createServer((req, res) => {
   // YOUR CODE GOES IN HERE
-  if (req.url === "/") {
-    fs.readFile(
-      path.join(__dirname, "public", "index.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end(content); // Ends the response
-      }
-    );
-  } else if (req.url === "/index.js") {
-    fs.readFile(path.join(__dirname, "public", "index.js"), (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { "content-type": "text/js" });
-      res.end(content); // Ends the response
-    });
-  } else if (req.url === "/style.css") {
-    fs.readFile(path.join(__dirname, "public", "style.css"), (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { "content-type": "text/css" });
-      res.end(content); // Ends the response
-    });
+
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  let extName = path.extname(filePath);
+
+  let contentType = "text/html";
+
+  switch (extName) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".html":
+      contentType = "text/html";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+    case ".jpeg":
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
   }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "no-one-there.html"),
+          (err, content) => {
+            res.writeHead(200, { contentType: "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        res.writeHead(500);
+        res.end(err.code);
+      }
+    } else {
+      res.writeHead(200, { "content-type": contentType });
+      res.end(content);
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
