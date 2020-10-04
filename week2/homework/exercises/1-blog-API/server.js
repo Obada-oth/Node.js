587db09f82092e3b5238e90161c85679116b0e0b
obada othman <obada.oth@gmail.com>
2020-10-04 15:59:22 +0200

    finished ex. Blog-API

--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -1,10 +1,67 @@
-const express = require('express')
+const express = require("express");
 const app = express();
- 
+const fs = require("fs");
+let { title } = require("process");
+let data = require("./posts.json");
+app.use(express.json());
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.post("/blogs", (req, res) => {
+  title = req.body.title;
+  content = req.body.content;
+  fs.writeFileSync(title, content);
+  data.push(req.body);
+
+  res.end("ok");
+});
+
+app.get("/blogs/:title", (req, res) => {
+  title = req.params.title;
+  content = req.body.content;
+
+  if (fs.existsSync(title)) {
+    const post = fs.readFileSync(title);
+
+    res.end(post);
+  } else {
+    res.status(404);
+    res.send("post is not found");
+  }
+});
+
+app.put("/posts/:title", (req, res) => {
+  title = req.params.title;
+  content = req.body.content;
+
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.end("ok");
+  } else {
+    res.send("title not found");
+  }
+});
+
+app.delete("/blogs/:title", (req, res) => {
+  title = req.params.title;
+
+  if (fs.existsSync(title)) {
+    fs.unlinkSync(title);
+
+    data.splice(data.indexOf(title), 1);
+
+    res.end("ok");
+  } else {
+    res.send("title not found");
+  }
+});
+
+app.get("/blogs", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(data);
+});
+
+app.listen(3000);
