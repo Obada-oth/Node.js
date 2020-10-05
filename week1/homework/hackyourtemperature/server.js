aedb79809662d46b57274db0433ebd9835c17d4e
obada othman <obada.oth@gmail.com>
2020-10-05 23:59:16 +0200

    Finished project HackYourTemperature for week2

---
+++ week1/homework/hackyourtemperature/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: false }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  console.log(cityName);
+  res.render("index", {
+    cityName,
+  });
+});
+
+// app.get("/", (req, res) => {
+//   res.send({ msg: "Hello from the backend to the client" });
+// });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
